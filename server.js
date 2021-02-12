b59a1512164bb1d7910edd8ee83c1cfe9f66c2c8
ohampton <44102431+ohampton@users.noreply.github.com>
2021-02-12 17:17:24 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,16 @@
 
 const express = require('express');
 const bodyParser= require('body-parser')
+const MongoClient = require('mongodb').MongoClient
 const app = express();
+
+const connectionString = "mongodb+srv://Toph:Beifong@cluster0.qzaij.mongodb.net/test?retryWrites=true&w=majority"
+
+MongoClient.connect(connectionString, { useUnifiedTopology: true })
+  .then(client => {
+    console.log('Connected to Database')
+    const db = client.db('star-wars-quotes')
+    const quotesCollection = db.collection('quotes')
 
 app.listen(3000, function() {
     console.log('listening on 3000')
@@ -10,10 +19,20 @@ app.listen(3000, function() {
 
 app.use(bodyParser.urlencoded({ extended: true }))
 
+app.post('/quotes', (req, res) => {
+    quotesCollection.insertOne(req.body)
+      .then(result => {
+        console.log(result)
+      })
+      .catch(error => console.error(error))
+  })
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
 })
 
 app.post('/quotes', (req, res) => {
     console.log(req.body)
+})
+  
   })
