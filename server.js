358462e4308d8c04a9e87aa7f17a1ee525b28a39
ohampton <44102431+ohampton@users.noreply.github.com>
2021-02-12 16:35:01 -0500

    commit 2

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
 // console.log('May the node be with you')
 
 const express = require('express');
+const bodyParser= require('body-parser')
 const app = express();
 
 app.listen(3000, function() {
     console.log('listening on 3000')
 })
-/
+
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
 })
 
 app.post('/quotes', (req, res) => {
-    console.log('Hellooooooooooooooooo!')
+    console.log(req.body)
   })
