c2900ac163c4818bad600a0b4142618eb30785e7
ohampton <44102431+ohampton@users.noreply.github.com>
2021-02-12 16:04:06 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+// console.log('May the node be with you')
+
+const express = require('express');
+const app = express();
+
+app.listen(3000, function() {
+    console.log('listening on 3000')
+})
+/
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
+app.post('/quotes', (req, res) => {
+    console.log('Hellooooooooooooooooo!')
+  })
